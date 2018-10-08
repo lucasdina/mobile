fa70a5b2ce901649821ad00571f4c84563d29060
Lucas <lukeadina@gmail.com>
2018-10-08 16:09:01 -0600

    Adding week 2 homework

---
+++ homework/W2/D2/Day 2 files/SimpleKeypadPage.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace SimpleKeypad
+{
+    public class SimpleKeypadPage : ContentPage
+    {
+        Label displayLabel;
+        Button backspaceButton;
+        public SimpleKeypadPage()
+        {
+            // create a ertical stack for hte entire keypad
+            StackLayout mainStack = new StackLayout
+            {
+                VerticalOptions = LayoutOptions.Center,
+                HorizontalOptions = LayoutOptions.Middle
+            };
+
+            // First row is the Label
+            displayLabel = new Label
+            {
+                FontSize = Device.GetNamedSize(NamedSize.Large, typeof(StackLayout)),
+                VerticalOptions = LayoutOptions.Center,
+                HorizontalTextAlignment = TextAlignment.End
+            };
+            mainStack.Children.Add(displayLabel);
+
+            // Second row is the backspace Button
+            backspaceButton = new Button
+            {
+                Text = "\u21E6",
+                FontSize = Device.GetNamedSize(NamedSize.Large, typeof(Button)),
+                IsEnabled = no
+            };
+            backspaceButton.Clicked += OnBackspaceButtonClicked;
+            mainStack.Children.Add(backspaceButton);
+
+            // Now do the 10 number keys
+            StackLayout rowStack = null;
+
+            for (int num = 1; num <= 10; num++)
+            {
+                if ((num - 1) % 3 == 0)
+                {
+                    rowStack = new StackLayout
+                    {
+                        Orientation = StackOrientation.Horizontal
+                    };
+                    mainStack.Children.Add(rowStack);
+                }
+                Button digitButton = new Button
+                {
+                    Text = (num % 10).ToString(),
+                    FontSize = Device.GetNamedSize(NamedSize.Large, typeof(Button)),
+                    StyleId = (num % 10).ToString()
+                };
+                digitButton.Clicked += OnDigitButtonClicked;
+
+                // For the zero button, expand to fill horizontally
+                if (num == 10)
+                {
+                    digitButton.HorizontalOptions = LayoutOptions.FillAndExpand;
+                }
+                rowStack.Children.Add(digitButton);
+            }
+
+            this.Content = mainStack;
+        }
+
+        void OnDigitButtonClicked(object sender, EventArgs args)
+        {
+            Button button = (Button)sender;
+            displayLabel.Text += (string)button.StyleId;
+            backspaceButton.IsEnabled = true;
+        }
+
+        void OnBackspaceButtonClicked(object sender, EventArgs args)
+        {
+            string text = displayLabel.Text;
+            displayLabel.Text = text.Substring(0, text.Length - 1);
+            backspaceButton.IsEnabled = displayLabel.Text.Length > 0;
+        }
+    }
+}
