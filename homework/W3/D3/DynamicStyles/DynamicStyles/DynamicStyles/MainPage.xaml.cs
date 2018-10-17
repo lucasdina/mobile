0757a53d2193bb904834696566211d52fe29d2ca
Lucas <lukeadina@gmail.com>
2018-10-16 22:20:33 -0600

    W3D3 Part 3 done.

---
+++ homework/W3/D3/DynamicStyles/DynamicStyles/DynamicStyles/MainPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace DynamicStyles
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        void OnButton1Clicked(object sender, EventArgs args)
+        {
+            Resources["buttonStyle"] = Resources["buttonStyle1"];
+        }
+
+        void OnButton2Clicked(object sender, EventArgs args)
+        {
+            Resources["buttonStyle"] = Resources["buttonStyle2"];
+        }
+
+        void OnButton3Clicked(object sender, EventArgs args)
+        {
+            Resources["buttonStyle"] = Resources["buttonStyle3"];
+        }
+
+        void OnResetButtonClicked(object sender, EventArgs args)
+        {
+            Resources["buttonStyle"] = null;
+        }
+    }
+}
