f7b2dbfe51d2bd76b30c634811b6820705db188b
Lucas <lukeadina@gmail.com>
2018-10-16 20:31:04 -0600

    iOS mtgws done

--- homework/W3/MonkeyTapGame/MonkeyTapGame/MonkeyTapGame.iOS/PlatformSoundPlayer.cs
+++ homework/W3/MonkeyTapGame/MonkeyTapGame/MonkeyTapGame.iOS/PlatformSoundPlayer.cs
@@ -1,14 +1,50 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
+﻿using System.Text;
 using Foundation;
-using UIKit;
+using Xamarin.Forms;
+using AVFoundation;
+using System.IO;
 
+[assembly: Dependency(typeof(MonkeyTapGame.iOS.PlatformSoundPlayer))]
 namespace MonkeyTapGame.iOS
 {
-    class PlatformSoundPlayer
+    public class PlatformSoundPlayer : IPlatformSoundPlayer
     {
+        const int numChannels = 1;
+        const int bitsPerSample = 16;
+        public void PlaySound(int samplingRate, byte[] pcmData)
+        {
+            int numSamples = pcmData.Length / (bitsPerSample / 8);
+
+            MemoryStream memoryStream = new MemoryStream();
+            BinaryWriter writer = new BinaryWriter(memoryStream, Encoding.ASCII);
+
+            // Construct WAVE header
+            writer.Write(new char[] { 'R', 'I', 'F', 'F', });
+            writer.Write(36 + sizeof(short) * numSamples);
+            writer.Write(new char[] { 'W', 'A', 'V', 'E', });
+            writer.Write(new char[] { 'f', 'm', 't', ' ', });
+            writer.Write(16);
+
+            writer.Write((short)1);
+            writer.Write((short)numChannels);
+            writer.Write(samplingRate);
+
+            writer.Write(samplingRate * numChannels * bitsPerSample / 8);
+            writer.Write((short)(numChannels * bitsPerSample / 8));
+
+            writer.Write((short)bitsPerSample);
+
+            writer.Write(new char[] { 'd', 'a', 't', 'a', });
+            writer.Write(numSamples * numChannels * bitsPerSample / 8);
+
+            // Write data as well
+            writer.Write(pcmData, 0, pcmData.Length);
+
+            memoryStream.Seek(0, SeekOrigin.Begin);
+            NSData data = NSData.FromStream(memoryStream);
+            AVAudioPlayer audioPlayer = AVAudioPlayer.FromData(data);
+            audioPlayer.Play();
+
+        }
     }
 }
