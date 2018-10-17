4aab3f986c694445e8e2f5fe3b4652020d237f8a
Lucas <lukeadina@gmail.com>
2018-10-16 20:34:40 -0600

    droid monkeytap done.

--- homework/W3/MonkeyTapGame/MonkeyTapGame/MonkeyTapGame/IPlatformSoundPlayer.cs
+++ homework/W3/MonkeyTapGame/MonkeyTapGame/MonkeyTapGame/IPlatformSoundPlayer.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace MonkeyTapGame
 {
-    class IPlatformSoundPlayer
+    public interface IPlatformSoundPlayer
     {
         void PlaySound(int samplingRate, byte[] pcmData);
 
