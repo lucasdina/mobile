8b280dee1652b5f32b01dad7c97287025dab228b
Lucas <lukeadina@gmail.com>
2018-10-16 19:38:32 -0600

    W3D1 homework part 1.

---
+++ homework/W3/D1/XamlKeypad/XamlKeypad/XamlKeypad/App.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+namespace XamlKeypad
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+
+        public string DisplayLabelText { set; get; }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
