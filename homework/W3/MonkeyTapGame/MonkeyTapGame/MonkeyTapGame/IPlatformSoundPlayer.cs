472e6a0709572033e11bf23d6636fa4cb240777c
Lucas <lukeadina@gmail.com>
2018-10-16 20:26:48 -0600

    Working on iOS monkey sound player.

---
+++ homework/W3/MonkeyTapGame/MonkeyTapGame/MonkeyTapGame/IPlatformSoundPlayer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MonkeyTapGame
+{
+    class IPlatformSoundPlayer
+    {
+        void PlaySound(int samplingRate, byte[] pcmData);
+
+    }
+}
