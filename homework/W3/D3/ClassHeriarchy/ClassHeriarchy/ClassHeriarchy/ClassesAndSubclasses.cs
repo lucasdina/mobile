965b366c15f5bf52a69519b1b64a11538bab1b1f
Lucas <lukeadina@gmail.com>
2018-10-16 22:07:20 -0600

    W3D1 Part 1 done

---
+++ homework/W3/D3/ClassHeriarchy/ClassHeriarchy/ClassHeriarchy/ClassesAndSubclasses.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassHeriarchy
+{
+    class ClassesAndSubclasses
+    {
+        public ClassesAndSubclasses(Type parent, bool isXamarinForms)
+        {
+            Type = parent;
+            IsXamarinForms = isXamarinForms;
+            Subclasses = new List<ClassesAndSubclasses>();
+        }
+
+        public Type Type { get; private set; }
+        public bool IsXamarinForms { get; private set; }
+        public List<ClassesAndSubclasses> Subclasses { get; private set; }
+    }
+}
