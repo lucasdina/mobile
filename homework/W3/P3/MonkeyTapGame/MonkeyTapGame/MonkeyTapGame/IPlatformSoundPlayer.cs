c9f4ab24239238feeccea340bcb4c33aff812ab8
Lucas <lukeadina@gmail.com>
2018-10-16 23:36:25 -0600

    Frick this fricken project...

---
+++ homework/W3/P3/MonkeyTapGame/MonkeyTapGame/MonkeyTapGame/IPlatformSoundPlayer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MonkeyTapGame
+{
+    public interface IPlatformSoundPlayer
+    {
+        void PlaySound(int samplingRate, byte[] pcmData);
+
+    }
+}
