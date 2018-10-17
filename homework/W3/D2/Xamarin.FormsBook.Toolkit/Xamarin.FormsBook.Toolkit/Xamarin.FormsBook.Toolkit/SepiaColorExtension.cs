ff1ee48b96318da10c3d7e2223ae1b01da988ece
Lucas <lukeadina@gmail.com>
2018-10-16 21:37:10 -0600

    Finished W3D2

---
+++ homework/W3/D2/Xamarin.FormsBook.Toolkit/Xamarin.FormsBook.Toolkit/Xamarin.FormsBook.Toolkit/SepiaColorExtension.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Xamarin.FormsBook.Toolkit
+{
+    public static class Toolkit
+    {
+        public static void Init()
+        {
+
+        }
+    }
+
+    public class SepiaColorExtension : IMarkupExtension
+    {
+        public SepiaColorExtension()
+        {
+            A = 1;
+        }
+
+        public double R { set; get; }
+
+        public double G { set; get; }
+
+        public double B { set; get; }
+
+        public double A { set; get; }
+
+        public object ProvideValue(IServiceProvider service)
+        {
+            return Color.FromRgba(R, G, B, A);
+        }
+    }
+}
