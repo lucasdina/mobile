86e7a63667935f6fac569d3a70844edebc4a0646
Lucas <lukeadina@gmail.com>
2018-11-01 13:50:41 -0600

    week 6 stuffs

---
+++ homework/W4/D3/CheckboxDemo/CheckboxDemo/CheckboxDemo/MainPage.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace CheckboxDemo
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        void OnItalicCheckBoxChanged(object sender, bool isChecked)
+        {
+            if (isChecked)
+            {
+                label.FontAttributes |= FontAttributes.Italic;
+            }
+            else
+            {
+                label.FontAttributes &= ~FontAttributes.Italic;
+            }
+
+        }
+
+        void OnBoldCheckBoxChanged(object sender, bool isChecked)
+        {
+            if (isChecked)
+            {
+                label.FontAttributes |= FontAttributes.Bold;
+            }
+            else
+            {
+                label.FontAttributes &= ~FontAttributes.Bold;
+            }
+
+        }
+
+    }
+}
---
+++ homework/W6/D1/Week6Day1Files/Week6Day1Files/DataTransfer5/App.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace DataTransfer5
+{
+	public partial class App : Application
+	{
+		public App ()
+		{
+            Xamarin.FormsBook.Toolkit.Toolkit.Init();
+
+            // Instantiate AppData and set property.
+            AppData = new AppData();
+
+            // Go to the home page.
+            MainPage = new NavigationPage(new DataTransfer5HomePage());
+        }
+
+        public AppData AppData { private set; get; }
+
+        protected override void OnStart ()
+		{
+			// Handle when your app starts
+		}
+
+		protected override void OnSleep ()
+		{
+			// Handle when your app sleeps
+		}
+
+		protected override void OnResume ()
+		{
+			// Handle when your app resumes
+		}
+	}
+}
